1c8b1a7f32af806ade9cd5f6a71bd59496fb5c9b
Валерий Мельцов <vinossis1993@gmail.com>
2020-02-22 00:59:42 +0300

    add message for empty user in chanel

--- src/components/containers/users/Users.js
+++ src/components/containers/users/Users.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
+import messages from '../../utils/messages';
+
 function Users({ chanelInfo }) {
   const [info, setInfo] = useState(chanelInfo);
   useEffect(() => {
@@ -14,6 +16,7 @@ function Users({ chanelInfo }) {
             <a href="#123">{item.name}</a>
           </li>
         ))}
+        {info && info.activeUsers.length === 0 ? <p>{messages.USER_LIST_EMPTY}</p> : ''}
       </ul>
     </div>
 
--- src/components/utils/messages.js
+++ src/components/utils/messages.js
@@ -1,3 +1,4 @@
 export default {
   EXIT_CHANEL: 'Покинуть канал',
+  USER_LIST_EMPTY: 'Список пуст',
 };
