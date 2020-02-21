54d6fe52664a10070fa809f989991f6533d45068
Валерий Мельцов <vinossis1993@gmail.com>
2020-02-21 22:23:39 +0300

    default menu

--- src/components/containers/menu/Menu.js
+++ src/components/containers/menu/Menu.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const defChanel = [
+  { chanelId: '1', name: 'csruhub' },
+  { chanelId: '2', name: 'hydramist' },
+  { chanelId: '3', name: 'rubendahouse' },
+  { chanelId: '4', name: 'nurseos' }];
 
 function Menu() {
+  const [chanels] = useState(defChanel);
+  const [currentSelectedChanel, setCurrentSelectedChanel] = useState('');
   return (
-    <div className="menu">2</div>
+    <div className="menu">
+      <ul>
+        {chanels && (
+          chanels.map((chanel) => (
+            <li key={chanel.chanelId}>
+              <a
+                className={chanel.chanelId === currentSelectedChanel ? 'active' : ''}
+                href='#'
+                onClick={() => setCurrentSelectedChanel(chanel.chanelId)}
+              >
+                {chanel.name}
+              </a>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
   );
 }
 
