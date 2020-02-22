25d622297dc4cd5e199319e6a6a3ea8b89dbff2b
Валерий Мельцов <vinossis1993@gmail.com>
2020-02-22 10:50:19 +0300

    add chat_form

--- src/components/containers/chat/Chat.js
+++ src/components/containers/chat/Chat.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import messages from '../../utils/messages';
 
 function Chat() {
   return (
-    <div className="chat">3</div>
+    <div className="chat_wrapper">
+      <div className="chat_window">
+        123
+      </div>
+      <div className="chat_form">
+        <div className="chat_form_area">
+          <textarea id="" name="" />
+        </div>
+        <div className="chat_form_submit">
+          <button>{messages.SEND_MESSAGE}</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/utils/messages.js
+++ src/components/utils/messages.js
@@ -2,4 +2,5 @@ export default {
   EXIT_CHANEL: 'Покинуть канал',
   USER_LIST_EMPTY: 'Список пуст',
   CREATE_CHANEL: 'Создать канал',
+  SEND_MESSAGE: 'Отправить',
 };
